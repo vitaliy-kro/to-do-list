c4435ba1b4720aad1501e3304a84a83439570e7e
vitaliy-kro <krochakv6@gmail.com>
2022-09-12 14:16:17 +0300

    adding some webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: path.resolve(__dirname, "./src/index.js"),
   output: { path: path.resolve(__dirname, "build"), filename: "main.js" },
+
+  mode: "development",
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, "/dist"),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 7777,
+  },
 
   plugins: [
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, "./src/index.html"),
       filename: "index.html",
     }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 
   module: {
